dd04c302c5c897f50f4d2df413cdb0c8e936d2be
gupaki09-art <gupaki09@gmail.com>
2026-09-23 18:08:47 -0400

    Refresh service worker cache handling

--- sw.js
+++ sw.js
@@ -1,1 +1,27 @@
-const C='tipaq-v4';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png']))));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
+const C='tipaq-v5';
+const CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+
+self.addEventListener('install',event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(C).then(cache=>cache.addAll(CORE)));
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(keys.filter(key=>key!==C).map(key=>caches.delete(key))))
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch',event=>{
+  const url=new URL(event.request.url);
+  if(url.pathname.startsWith('/api/')){
+    event.respondWith(fetch(event.request));
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request).catch(()=>caches.match(event.request))
+  );
+});
